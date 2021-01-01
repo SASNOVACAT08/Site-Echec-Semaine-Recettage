90041833b06149e0dd53f69da6086a4e0fe9cdfb
unknown <theo090992@yahoo.fr>
2021-01-01 20:14:28 +0100

    2 => modification js

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 
 let menuOpen = false;
 
+/*____________*/
+
+window.onresize = resize;
+
+function resize(){
+  console.log(window.innerWidth);
+  if (window.innerWidth > 800) {
+    document.getElementById('navigation').classList.remove('navigation_edit');
+    for (let i = 0; i <= document.getElementsByClassName('button_nav').length - 1; i++){
+      document.getElementsByClassName('button_nav')[i].classList.remove('button_nav_edit')
+    }
+    menuOpen = false;
+  }
+}
+
+/*____________*/
+
 document.getElementById('menuOpen').addEventListener('click', function(e) {
   if (menuOpen === false){
-    document.getElementById('navigation').style.height = '220px';
+    document.getElementById('navigation').classList.add('navigation_edit');
     for (let i = 0; i <= document.getElementsByClassName('button_nav').length - 1; i++){
-      document.getElementsByClassName('button_nav')[i].style.display = 'block';
+      document.getElementsByClassName('button_nav')[i].classList.add('button_nav_edit')
     }
     menuOpen = true;
   }
   else {
-    document.getElementById('navigation').style.height = '60px';
+    document.getElementById('navigation').classList.remove('navigation_edit');
     for (let i = 0; i <= document.getElementsByClassName('button_nav').length - 1; i++){
-      document.getElementsByClassName('button_nav')[i].style.display = 'none';
+      document.getElementsByClassName('button_nav')[i].classList.remove('button_nav_edit')
     }
     menuOpen = false;
   }
